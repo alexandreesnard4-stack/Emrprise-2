64c8d0da352aae44ecde4d1583f4d1d7e5192dc3
alexandreesnard4-stack <alexandreesnard4@gmail.com>
2026-08-17 12:14:43 +0200

    Add files via upload

--- emprise-app/src/firebase.js
+++ emprise-app/src/firebase.js
@@ -2,6 +2,7 @@
 // (voir .env.example). Ne jamais coder les clés en dur dans ce fichier.
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -14,3 +15,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
